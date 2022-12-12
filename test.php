23bc1e4d4ea9300afeef5fc7ca5ae51970ccc6d4
amitnashikkar <bill.siivo@gmail.com>
2022-12-12 05:43:13 -0800

    first commit

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+$input="My name is Virat";
+
+$temp=substr($input,10,8);
+$temp2=substr($input,7,3);
+
+echo "<br>".$temp;
+echo "<br>".$temp2;
+
+
+echo "<br>".$temp."".$temp2;
+
+$tmp=explode(" ",$input);
+
+echo "<pre>";
+print_r($tmp[3]);
+//echo strlen($input);
+
+//Virat is name My
+?>
